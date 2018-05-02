6eb79608231263773dd24bd16bba7c3d7227558e
Sudipta Chakraborty <sudiptachakraborty@Sudiptas-MacBook-Air.local>
2018-05-02 12:41:36 -0700

    Csudipta

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = 3000;
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+});
+ 
+app.get('/data',function(req,res){
+	res.send('data')
+})
+app.listen(port  , function(){
+	console.log("Server listening on: ", port  )
+})
